43ec28e33ee9f559c6c3774d5c92ee3291299f53
JZ-Zhou-UofC <john.zhou1@ucalgary.ca>
2026-02-15 11:40:38 -0700

    sword animation

--- scenes/actors/player/player.gd
+++ scenes/actors/player/player.gd
@@ -1,30 +1,89 @@
 extends CharacterBody2D
 
+#################################################
+# MOVEMENT
+#################################################
+
 @export var speed := 300.0
 
 @onready var body = $Body
+
+
+#################################################
+# WEAPON
+#################################################
+
+@onready var weapon_pivot = $WeaponPivot
+@onready var weapon_sprite = $WeaponPivot/WeaponSprite
+
+var weapon_textures = {
+	"A": preload("res://assets/weapons/weapon_A.png"),
+	"B": preload("res://assets/weapons/weapon_B.png"),
+	"C": preload("res://assets/weapons/weapon_C.png")
+}
+
+
+#################################################
+# PORTRAITS
+#################################################
+
 var portraits = {
 	GameState.LifeStage.BABY: preload("res://assets/characters/brotato/baby.png"),
 	GameState.LifeStage.TEEN: preload("res://assets/characters/brotato/teen.png"),
 	GameState.LifeStage.PRIME: preload("res://assets/characters/brotato/prime.png"),
 	GameState.LifeStage.OLD: preload("res://assets/characters/brotato/old.png")
 }
+
+
+#################################################
+# SWING SYSTEM (AUTO)
+#################################################
+
+var swing_speed = 6.0
+var swing_angle = 70.0
+var swing_time = 0.0
+
+
+#################################################
+# READY
+#################################################
+
 func _ready():
 
 	load_from_gamestate()
+	update_weapon()
 
+
+#################################################
+# LOAD PLAYER DATA
+#################################################
 
 func load_from_gamestate():
 
-	# load stats
 	speed = GameState.player["speed"]
 
-	# load portrait
 	var stage = GameState.life_stage
 
 	if portraits.has(stage):
 		body.texture = portraits[stage]
+
+
+#################################################
+# PHYSICS
+#################################################
+
 func _physics_process(delta):
+
+	handle_movement(delta)
+	update_weapon_rotation()
+	update_auto_swing(delta)
+
+
+#################################################
+# MOVEMENT
+#################################################
+
+func handle_movement(delta):
 
 	var direction = Input.get_vector(
 		"move_left",
@@ -33,11 +92,10 @@ func _physics_process(delta):
 		"move_down"
 	)
 
-	# movement
 	velocity = direction * speed
+
 	move_and_slide()
 
-	# flip body left/right only
 	update_facing(direction)
 
 
@@ -48,3 +106,40 @@ func update_facing(direction):
 
 	elif direction.x < 0:
 		body.flip_h = true
+
+
+#################################################
+# WEAPON SETUP
+#################################################
+
+func update_weapon():
+
+	var weapon_type = GameState.player["weapon_type"]
+
+	if weapon_textures.has(weapon_type):
+
+		weapon_sprite.texture = weapon_textures[weapon_type]
+
+
+#################################################
+# AIM SYSTEM
+#################################################
+
+func update_weapon_rotation():
+
+	var mouse_pos = get_global_mouse_position()
+
+	weapon_pivot.look_at(mouse_pos)
+
+
+#################################################
+# AUTO SWING SYSTEM
+#################################################
+
+func update_auto_swing(delta):
+
+	swing_time += delta * swing_speed
+
+	var swing_offset = sin(swing_time) * swing_angle
+
+	weapon_sprite.rotation_degrees = swing_offset
