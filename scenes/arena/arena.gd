8b05e60534050baf0988ed6a490b88842fad1c63
JZ-Zhou-UofC <john.zhou1@ucalgary.ca>
2026-02-15 13:13:54 -0700

    pass and fail button

--- scenes/arena/arena.gd
+++ scenes/arena/arena.gd
@@ -119,3 +119,17 @@ func spawn_boss():
 func spawn_normal_enemies():
 
 	print("Spawning normal enemies")
+	
+func _on_pass_button_pressed():
+		handle_stage_end(true)
+func _on_fail_button_pressed():
+		handle_stage_end(false)
+		
+func handle_stage_end(passed: bool):
+	if passed:
+		GameState.gain_energy(4)
+	else:
+		GameState.gain_energy(3)
+		
+		GameState.advance_turn()
+		SceneManager.goto_hub()
